72af5185aeb6a0207b051b492ccffc35877651f7
Stefan Weiz <sweiz@pc-bn-063.lan.tarent.de>
2014-07-22 10:50:39 +0200

    Add new projectwq

---
+++ src/main/java/com/stef/game/map/BuildMap.java
@@ -0,0 +1,12 @@
+package com.stef.game.map;
+
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+
+public class BuildMap {
+    Scanner wh = new Scanner(System.in);
+    
+    int width = wh.nextInt();
+    int height = wh.nextInt();
+}
